effa0bed77076f73852e184578524e68a25243d7
Dodoh04 <douglasoliversa3@gmail.com>
2023-02-07 20:25:50 -0300

    Iniciando ListView

--- app/src/main/java/br/com/etecia/catalogo/MainActivity.java
+++ app/src/main/java/br/com/etecia/catalogo/MainActivity.java
@@ -3,8 +3,11 @@ package br.com.etecia.catalogo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView idListCamiseta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
