93104c50480fbec6e8c95c19ec971d6017c3f87a
Dodoh04 <douglasoliversa3@gmail.com>
2023-02-14 22:11:13 -0300

    fim da apresentação e repetição do card no java e add das imagens na variavel

--- app/src/main/java/br/com/etecia/catalogo/MainActivity.java
+++ app/src/main/java/br/com/etecia/catalogo/MainActivity.java
@@ -6,11 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView idListCamisetas;
+    String titulo[] = {};
+    String desc1 [] = {};
+    String desc2 [] = {};
+    String pontos [] = {};
+    int Camisetas [] = {R.drawable.camisetaum, R.drawable.camisetadois, R.drawable.camisetatres, R.drawable.camisetaquatro, R.drawable.camisetacinco,
+    R.drawable.camisetaseis, R.drawable.camisetasete, R.drawable.camisetaoito};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 0;
+            return Camisetas.length;
         }
 
         @Override
@@ -41,8 +49,31 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+            //variaveis do modelo
+            ImageView imagemCamisetas;
+            TextView descricao1Camisetas, descricao2Camisetas, pontuacao, tituloCamisetas;
+
+            //adaptador ligando ao modelo
+
+            View v = getLayoutInflater().inflate(R.layout.modelo_camisetas, null);
+
+            //Apresentar as Variaveis do java para o modelo xml3
+
+            imagemCamisetas = v.findViewById(R.id.idImgCamiseta);
+            tituloCamisetas = v.findViewById(R.id.idTitulo);
+            descricao1Camisetas = v.findViewById(R.id.idDescricao1);
+            descricao2Camisetas = v.findViewById(R.id.idDescricao2);
+            pontuacao = v.findViewById(R.id.idPontuacao);
+
+
+            tituloCamisetas.setText(titulo[i]);
+            descricao1Camisetas.setText(desc1[i]);
+            descricao2Camisetas.setText(desc2[i]);
+            pontuacao.setText(pontos[i]);
+            imagemCamisetas.setImageResource(Camisetas[i]);
+
+            return v;
         }
     }
-    public View getView( int i, )
+
 }
